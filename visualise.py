95c13e524b2a954c61a1de9fd9724b612711b0d0
Lasse Wolter <wolter.lasse@gmail.com>
2022-02-23 14:20:21 +0000

    feat(visalise.py): improved visualisation showing 1 plots for each metric
    + 1 plot for each metric (loss, acc, prec, recall) comparing performance
      on training and validation set

--- visualise.py
+++ visualise.py
@@ -22,20 +22,44 @@ def plot_train_metrics(df):
     Columns of df are expected to be:
     [ batch_num,train_prec,train_rec,train_acc,train_loss,val_prec,val_rec,val_acc,val_loss ] 
     """
-    split = 'val'
+
+    # Tupel of split name and the plt representation that should be used for this split
+    splits = [('train', 'b--'),('val', 'r--')]
+    num_train_samples = 91000 
+    batch_size = 32
+    batches_per_epoch = num_train_samples/float(batch_size)
+    np.linspace(0,num_train_samples*4, 4)
+
+    # Functions used to convert between primary and secondary axis
+    def _to_epoch(x):
+        return x / batches_per_epoch 
+
+    def _to_batch_num(x):
+        return x* batches_per_epoch 
+
+    fig, axs = plt.subplots(2,2)
 
     
-    smooth_x, smooth_y = smooth(df.batch_num, df.val_prec, 50)
-    # plt.plot(df['batch_num'], df[f'{split}_prec'], 'b--')
-    # plt.plot(df['batch_num'], df[f'{split}_rec'],  'r--')
-    # plt.plot(df['batch_num'], df[f'{split}_acc'], 'c--')
-    # plt.plot(df['batch_num'], df[f'{split}_loss'], 'g--')
-    plt.plot(smooth_x,smooth_y, 'c--')
-    plt.ylabel('Performance')
-    plt.xlabel('Processed batches')
+    for split,plt_rep in splits:
+        axs[0,0].plot(df['batch_num'], df[f'{split}_prec'], plt_rep, label=f'{split}_precision')
+        axs[0,1].plot(df['batch_num'], df[f'{split}_rec'],  plt_rep, label=f'{split}_recall')
+        axs[1,0].plot(df['batch_num'], df[f'{split}_acc'], plt_rep, label=f'{split}_accuracy')
+        axs[1,1].plot(df['batch_num'], df[f'{split}_loss'], plt_rep, label=f'{split}_loss')
+
+    axs[0,0].set_title('Precision')
+    axs[0,1].set_title('Recall')
+    axs[1,0].set_title('Accuracy')
+    axs[1,1].set_title('Loss')
+    for ax in axs.reshape(-1):
+        ax.legend()
+        ax.set_ylabel('Performance')
+        ax.set_xlabel('Processed batches')
+        secax = ax.secondary_xaxis('top', functions=(_to_epoch, _to_batch_num))
+        secax.set_xlabel('epochs')
+
     plt.show()
 
 
 if __name__ == '__main__':
-    metrics_df = pd.read_csv('./results/1_to_1_15_02/metrics.csv')
+    metrics_df = pd.read_csv('./train_results/tmp/metrics.csv')
     plot_train_metrics(metrics_df)
