4fc0cee533594eca41e4446fa66a2fe94abae57e
Lasse Wolter <wolter.lasse@gmail.com>
2022-03-22 15:46:05 +0000

    feat(visualise): updated to plot prec-recall and conf-matrix for several experiments in one run

--- visualise.py
+++ visualise.py
@@ -83,14 +83,15 @@ def plot_prec_recall_curve(dfs_with_labels, out_name='prec_recall_curve.png', sp
 
     fig, axs = plt.subplots()
 
-    cols = ['b', 'g', 'r']
+    cols = ['b', 'g', 'r', 'c']
+    if len(dfs_with_labels) > len(cols): print('More plots than colours, colours will be repeated')
     for idx, (df, label) in enumerate(dfs_with_labels):
         if 'recall' not in df.columns or 'precision' not in df.columns:
             raise LookupError(
                 f'Missing precision or recall column in passed dataframe. Found columns: {df.columns}')
 
-        axs.plot(df['recall'], df['precision'], f'{cols[idx]}--', label=label)
-        axs.plot(df['recall'], df['precision'], f'{cols[idx]}o')
+        axs.plot(df['recall'], df['precision'], f'{cols[idx%len(cols)]}--', label=label)
+        axs.plot(df['recall'], df['precision'], f'{cols[idx%len(cols)]}o')
     axs.set_ylabel('Precision')
     axs.set_xlabel('Recall')
     axs.set_title(split)
@@ -100,15 +101,14 @@ def plot_prec_recall_curve(dfs_with_labels, out_name='prec_recall_curve.png', sp
     if(show):
         plt.show()
 
-def plot_conf_matrix(eval_df_path, name='conf_matrix', thresholds=[], min_len=None, show=False):
+def plot_conf_matrix(eval_df_path, split, name='conf_matrix', thresholds=[], min_len=None, show=False):
     '''
     Calculate and plot confusion matrix across all meetings per parameter set
     You can specify thresholds(several) and min_len(one) which you want to include
     If nothing passed, all thresholds and min_lens will be plotted
     '''
     path = Path(eval_df_path)
-    split = path.name
-    eval_df = pd.read_csv(path.parent / f"{split}_{cfg.ANALYSIS['eval_df_cache_file']}")
+    eval_df = pd.read_csv(path / f"{split}_{cfg.ANALYSIS['eval_df_cache_file']}")
     sum_vals = eval_df.groupby(['threshold', 'min_len'])[['corr_pred_time', 'tot_pred_time', 'tot_transc_laugh_time', 'tot_fp_speech_time', 'tot_fp_noise_time', 'tot_fp_silence_time']].agg(['sum']).reset_index()
 
     # Flatten Multi-index to Single-index
@@ -159,43 +159,37 @@ def compare_num_of_val_batches():
     more_batches_df = pd.read_csv('./results/1_to_10_23_02/metrics.csv')
     plot_train_metrics(more_batches_df, name='few_batches_df', out_dir=out_dir)
 
-def compare_prec_recall(dirs_with_lables, show=False):
+def compare_prec_recall(dirs_with_labels, min_len, split, show=False):
     '''
     Compare the prec-recall curve of different experiments
     The input should be a list of tuples containing elements of (dir-path, label).
     Each dir-path needs to be a directory ending in 'pred' containing the predictions for each split 
     in a subfolder 'train', 'dev' and 'test'
+    - min_len: for which min_len parameter should the prec-recall curve be plotted
     '''
     dfs = []
-    split = 'dev'
-    for idx,(dir, label) in enumerate(dirs_with_lables):
+    for (dir, label) in dirs_with_labels:
         df = pd.read_csv(f"{dir}/{split}_{cfg.ANALYSIS['sum_stats_cache_file']}")
-        dfs.append((df, label)) 
-    plot_prec_recall_curve(dfs, split=split,  show=show)
-
-    dfs = []
-    split = 'train'
-    for idx,(dir, label) in enumerate(dirs_with_lables):
-        df = pd.read_csv(f"{dir}/{split}_{cfg.ANALYSIS['sum_stats_cache_file']}")
-        dfs.append((df, label)) 
-    plot_prec_recall_curve(dfs, split=split,  show=show)
-
-    # df1 = pd.read_csv('./results/1_to_10_23_02/preds/train_eval.csv')
-    # df2 = pd.read_csv('./results/1_to_20_06_03/preds/train_eval.csv')
-    # dfs = [(df1, '1_to_10'), (df2, '1_to_20')]
-    # plot_prec_recall_curve(dfs, split='train', show=False)
+        min_len_df = df[df.min_len == min_len].copy()
+        dfs.append((min_len_df, label)) 
+    plot_prec_recall_curve(dfs, out_name='compare_class_balance_dev_set', split=split,  show=show)
 
 if __name__ == '__main__':
     # compare_num_of_val_batches()
 
     # PREC-RECALL
     dirs = [
-        ('./results/1_to_10_23_02/preds', '1_to_10'),
-        ('./results/1_to_20_06_03/preds', '1_to_20')
+        './results/1_to_1_21_03/preds',
+        './results/1_to_10_16_03/preds',
+        './results/1_to_20_16_03/preds',
+        './results/1_to_20_struc_22_03/preds/'
      ]
-    compare_prec_recall(dirs, show=True)
-
+    labels = ['1_to_1', '1_to_10', '1_to_20', 'struc_1_to_20']
+    dirs_with_labels = list(zip(dirs, labels))
+    compare_prec_recall(dirs_with_labels, min_len=0.2, split='dev', show=True)
 
     # CONF-MATRIX
-    # thrs = np.linspace(0,1,11).round(2)
-    # plot_conf_matrix('./results/gillick_with_fixed_probs_20_03/sr_8000/preds/dev', 'test', thresholds=thrs, min_len=0.0, show=True)
+    thrs = np.linspace(0,1,11).round(2)
+    for dir, label in dirs_with_labels: 
+        plot_conf_matrix(dir, split='dev', name=label, thresholds=thrs, min_len=0.2, show=True)
+
